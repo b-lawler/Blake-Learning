7490c0940573522f4643aa030286316482b81168
Blake lawler <blake.lawler@gmail.com>
2015-02-24 00:26:15 -0500

    Problem 1 solution

---
+++ Project_Euler/1.js
@@ -0,0 +1,11 @@
+var sum=function(){
+    var total=0;
+    for(var i=0; i<1000;i++){
+        if(i%3===0 || i%5===0){
+            total+=i;
+        }
+    }
+    return total;
+};
+
+console.log(sum());
